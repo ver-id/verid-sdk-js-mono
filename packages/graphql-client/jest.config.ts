3a5ff95ca77f9d9d6cc830cf0086478229b2b7cb
Sten Reijers <stenreijers@gmail.com>
2026-03-11 12:03:59 +0100

    fix: attempt to go back to 24 without env and inline swcrc

--- packages/graphql-client/jest.config.ts
+++ packages/graphql-client/jest.config.ts
@@ -1,19 +1,24 @@
-import { readFileSync } from 'fs';
-
-// Reading the SWC compilation config for the spec files
-const swcJestConfig = JSON.parse(
-  readFileSync(`${import.meta.dirname}/.spec.swcrc`, 'utf-8')
-);
-
-// Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
-swcJestConfig.swcrc = false;
-
 export default {
   displayName: '@ver-id/graphql-client',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
+    '^.+\\.[tj]s$': [
+      '@swc/jest',
+      {
+        jsc: {
+          target: 'es2017',
+          parser: { syntax: 'typescript', decorators: true, dynamicImport: true },
+          transform: { decoratorMetadata: true, legacyDecorator: true },
+          keepClassNames: true,
+          externalHelpers: true,
+          loose: true,
+        },
+        module: { type: 'es6' },
+        sourceMaps: true,
+        exclude: [],
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: 'test-output/jest/coverage',
