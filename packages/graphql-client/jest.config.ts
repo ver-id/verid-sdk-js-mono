e6d6b0ec9b1e236b42eaf3b5a1e34ea66ab855e8
Sten Reijers <stenreijers@gmail.com>
2026-03-11 11:51:07 +0100

    fix: remove commonjs in jest

--- packages/graphql-client/jest.config.ts
+++ packages/graphql-client/jest.config.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs';
 
 // Reading the SWC compilation config for the spec files
 const swcJestConfig = JSON.parse(
-  readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8')
+  readFileSync(`${import.meta.dirname}/.spec.swcrc`, 'utf-8')
 );
 
 // Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
